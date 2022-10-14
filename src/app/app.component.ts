234b7c4b61cc472e229b94fcca1c6310b1844034
alessandro.carestia <carestiaal@gmail.com>
2022-10-14 17:54:43 +0200

    primo commit angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {MyButtonConfig} from "./my-button-config";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  myConfig: MyButtonConfig = new MyButtonConfig();
   title = 'noleggio-auto-angular';
+
+  constructor() {
+
+  }
+
+  ngOnInit() {
+
+  }
+
+
 }
---
+++ src/app/my-button-config.ts
@@ -0,0 +1,7 @@
+export class MyButtonConfig {
+
+  customCssClass: string | undefined;
+  text: string | undefined;
+  icon: string | undefined;
+
+}
